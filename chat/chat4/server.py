4239d5001fabfca8bbab6f2184a0c96bff367f0e
RaoufHegazy <93089962+RaoufHegazy@users.noreply.github.com>
2023-05-30 23:12:29 +0300

    Add files via upload

---
+++ chat/chat4/server.py
@@ -0,0 +1,43 @@
+import threading
+from socket import *
+from tkinter import *
+
+host = '127.0.0.1'
+port = 8888
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((host,port))
+s.listen()
+clients = []
+aliases = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def receive():
+    while True:
+        client,add = s.accept()
+        clients.append(client)
+        client.send('alias?'.encode('utf-8'))
+        alias = client.recv(1024)
+        aliases.append(alias)
+        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+def handle_client(client):
+    while True:
+        try:
+            m = client.recv(1024)
+            broadcast(m)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            alias = aliases[index]
+            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
+            aliases.remove(alias)
+            break
+
+if __name__ == "__main__":
+    receive()
